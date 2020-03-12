dcae665797d65e8f33a9676f6b005a8d1f065ec7
wangxiaobo33 <wangxiaobo9@jd.com>
2020-03-12 16:39:53 +0800

    操作

--- demos/demo.js
+++ demos/demo.js
@@ -1,16 +1,32 @@
 
-$(function () {
+var arView1, arView2;
+function loadDemo() {
 
-
-    var arView1, arView2;
+    // var arView1, arView2;
 
     arView1 = new ARView.Player('#view1', {
-        hideFullscreenButton: true,
-        image: './images/taj-mahal.jpg'
+        // hide_fullscreen_button: true,
+        image: './images/taj-mahal.jpg',
+        // is_vr_off: false,
+        // is_debug: false,
+        // is_autopan_off: false,
     })
 
-    arView1.onLoad()
-})
-// arView2 = new ARView.Player('#view2', {
-//     hideFullscreenButton: true
-// })
+    setTimeout(() => {
+        arView1.onLoad()
+    }, 0);
+
+
+    arView2 = new ARView.Player('#view2', {
+
+        image: './images/taj-mahal.jpg',
+
+    })
+
+    setTimeout(() => {
+
+        arView2.onLoad()
+    }, 0);
+}
+
+window.onload = loadDemo
